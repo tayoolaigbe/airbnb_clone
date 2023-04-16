4d65a93c07810bcdfcf5f27caddc05e5f261caf3
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-16 13:04:39 +0100

    Completed the Login functionality and fetched user from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
 import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
 	title: 'Airbnb',
@@ -15,18 +17,20 @@ const font = Nunito({
 	subsets: ['latin'],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const currentUser = await getCurrentUser();
 	return (
 		<html lang="en">
 			<body className={font.className}>
 				<ClientOnly>
 					<ToasterProvider />
 					<RegisterModal />
-					<Navbar />
+					<LoginModal />
+					<Navbar currentUser={currentUser} />
 				</ClientOnly>
 				{children}
 			</body>
