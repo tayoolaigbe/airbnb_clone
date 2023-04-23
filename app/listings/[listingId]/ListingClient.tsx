359e8c9090cb79eb551885b8d2d431b6aec70f1c
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-23 22:54:08 +0100

    Trying to fix the double reservation bug

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -6,7 +6,7 @@ import ListingInfo from '@/app/components/listings/ListingInfo';
 import ListingReservation from '@/app/components/listings/ListingReservation';
 import { categories } from '@/app/components/navbar/Categories';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { SafeListing, SafeUser, safeReservation } from '@/app/types';
+import { SafeListing, SafeUser, SafeReservation } from '@/app/types';
 import axios from 'axios';
 import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns';
 import { useRouter } from 'next/navigation';
@@ -21,7 +21,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-	reservations?: safeReservation[];
+	reservations?: SafeReservation[];
 	listing: SafeListing & {
 		user: SafeUser;
 	};
@@ -39,7 +39,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
 	const disabledDates = useMemo(() => {
 		let dates: Date[] = [];
 
-		reservations.forEach(reservation => {
+		reservations.forEach((reservation: any) => {
 			const range = eachDayOfInterval({
 				start: new Date(reservation.startDate),
 				end: new Date(reservation.endDate),
