498c811cab8b8fcd01f5e11054873c548733732e
tayoolaigbe <tayoolaigbe@outlook.com>
2023-04-28 21:13:16 +0100

    Modified Home to accept props and completed the properties loading screen with server components

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,21 @@
 import prisma from '@/app/libs/prismadb';
 
-export default async function getListings() {
+export interface IListingParams {
+	userId?: string;
+}
+
+export default async function getListings(params: IListingParams) {
 	try {
+		const { userId } = params;
+
+		let query: any = {};
+
+		if (userId) {
+			query.userId = userId;
+		}
+
 		const listings = await prisma.listing.findMany({
+			where: query,
 			orderBy: {
 				createdAt: 'desc',
 			},
@@ -13,6 +26,6 @@ export default async function getListings() {
 		}));
 		return safeListings;
 	} catch (error: any) {
-		throw new Error();
+		throw new Error(error);
 	}
 }
